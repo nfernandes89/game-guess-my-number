92a33b9d5c0fa66f58ec491080c1572c96c5239b
nfernandes89 <113728338+nfernandes89@users.noreply.github.com>
2022-11-29 11:42:29 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").innerHTML = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    displayMessage("⛔️ No number");
+  } else if (guess === secretNumber) {
+    displayMessage("🥳 Correct number");
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").innerHTML = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "⚠️ Too high!" : "⚠️ Too low!");
+      score--;
+      document.querySelector(".score").innerHTML = score;
+    } else {
+      displayMessage("💥 You lost the game");
+      document.querySelector(".score").innerHTML = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  displayMessage("Start guessing...");
+  document.querySelector(".score").innerHTML = score;
+  document.querySelector(".number").innerHTML = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
